ba7892fdc986495cd8275ff1a957937e1e0c28dd
Arthi-chaud <arthur.jamet@epitech.eu>
2021-12-21 15:37:43 +0100

    JSON Parser Tests: remove debug

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,13 +1,11 @@
 import Test.Framework (defaultMain)
 import TestBasicParser
 import TestJ4HSKL.Parser
-import Debug.Trace
 import Assets (loadTestAssets)
 
 main :: IO ()
 main = do
-    testAssets <- Assets.loadTestAssets 
-    traceIO $ show (map (\asset-> fst asset) (fst testAssets))
+    testAssets <- Assets.loadTestAssets
     defaultMain [ TestBasicParser.testSuite, TestJ4HSKL.Parser.testSuite testAssets ]
 
     
